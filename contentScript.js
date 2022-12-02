7363d03d42da9c12ddafec41b221016ca56c4dca
chemradio <chemradio@gmail.com>
2022-12-02 12:54:45 +0600

    kinda working need to populate the db

--- contentScript.js
+++ contentScript.js
@@ -1,78 +1,83 @@
 (() => {
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type, ads } = obj;
-    if (type === 'ready') {
-      setTimeout(removeAds, 3000, [ads]);
-    }
-  });
+	chrome.runtime.onMessage.addListener((obj, sender, response) => {
+		const { type, ads } = obj;
+		if (type === 'ready') {
+			removeAds(ads);
+			// setTimeout(removeAds, 3000, [ads]);
+		}
+	});
 
-  const removeAds = (ads) => {
-    iframes = document.querySelectorAll('iframe');
-    if (iframes) {
-      for (const iframe of iframes) {
-        iframe.parentElement.removeChild(iframe);
-      }
-    }
+	const removeAds = (ads) => {
+		for (const ad of ads) {
+			let elements = document.querySelectorAll(ad);
+			if (!elements) {
+				continue;
+			}
+			for (const element of elements) {
+				element.parentElement.removeChild(element);
+			}
+		}
+		removeIFrames();
+		setTimeout(removeIFrames, 2000);
+	};
 
-    for (const ad of ads) {
-      let elements = document.querySelectorAll(ad);
-      if (!elements) {
-        continue;
-      }
-      for (const element of elements) {
-        element.parentElement.removeChild(element);
-      }
-    }
-  };
+	const removeIFrames = () => {
+		iframes = document.querySelectorAll('iframe');
+		if (iframes) {
+			for (const iframe of iframes) {
+				iframe.parentElement.removeChild(iframe);
+			}
+		}
+	};
 
-  // let youtubeLeftControls, youtubePlayer;
-  // let currentVideo = '';
-  // let currentVideoBookmarks = [];
+	// let youtubeLeftControls, youtubePlayer;
+	// let currentVideo = '';
+	// let currentVideoBookmarks = [];
 
-  // const fetchBookmarks = () => {
-  //   return new Promise((resolve) => {
-  //     chrome.storage.sync.get([currentVideo], (obj) => {
-  //       resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
-  //     });
-  //   });
-  // };
+	// const fetchBookmarks = () => {
+	//   return new Promise((resolve) => {
+	//     chrome.storage.sync.get([currentVideo], (obj) => {
+	//       resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+	//     });
+	//   });
+	// };
 
-  // const newVideoLoaded = async () => {
-  //   const bookmarkBtnExists =
-  //     document.getElementsByClassName('bookmark-btn')[0];
-  //   currentVideoBookmarks = await fetchBookmarks();
+	// const newVideoLoaded = async () => {
+	//   const bookmarkBtnExists =
+	//     document.getElementsByClassName('bookmark-btn')[0];
+	//   currentVideoBookmarks = await fetchBookmarks();
 
-  //   if (!bookmarkBtnExists) {
-  //     const bookmarkBtn = document.createElement('img');
+	//   if (!bookmarkBtnExists) {
+	//     const bookmarkBtn = document.createElement('img');
 
-  //     bookmarkBtn.src = chrome.runtime.getURL('assets/bookmark.png');
-  //     bookmarkBtn.className = 'ytp-button ' + 'bookmark-btn';
-  //     bookmarkBtn.title = 'Click to bookmark current timestamp';
+	//     bookmarkBtn.src = chrome.runtime.getURL('assets/bookmark.png');
+	//     bookmarkBtn.className = 'ytp-button ' + 'bookmark-btn';
+	//     bookmarkBtn.title = 'Click to bookmark current timestamp';
 
-  //     youtubeLeftControls =
-  //       document.getElementsByClassName('ytp-left-controls')[0];
-  //     youtubePlayer = document.getElementsByClassName('video-stream')[0];
+	//     youtubeLeftControls =
+	//       document.getElementsByClassName('ytp-left-controls')[0];
+	//     youtubePlayer = document.getElementsByClassName('video-stream')[0];
 
-  //     youtubeLeftControls.append(bookmarkBtn);
-  //     bookmarkBtn.addEventListener('click', addNewBookmarkEventHandler);
-  //   }
-  // };
+	//     youtubeLeftControls.append(bookmarkBtn);
+	//     bookmarkBtn.addEventListener('click', addNewBookmarkEventHandler);
+	//   }
+	// };
 
-  // const addNewBookmarkEventHandler = async () => {
-  //   const currentTime = youtubePlayer.currentTime;
-  //   const newBookmark = {
-  //     time: currentTime,
-  //     desc: 'Bookmark at ' + getTime(currentTime),
-  //   };
+	// const addNewBookmarkEventHandler = async () => {
+	//   const currentTime = youtubePlayer.currentTime;
+	//   const newBookmark = {
+	//     time: currentTime,
+	//     desc: 'Bookmark at ' + getTime(currentTime),
+	//   };
 
-  //   currentVideoBookmarks = await fetchBookmarks();
+	//   currentVideoBookmarks = await fetchBookmarks();
 
-  //   chrome.storage.sync.set({
-  //     [currentVideo]: JSON.stringify(
-  //       [...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)
-  //     ),
-  //   });
-  // };
+	//   chrome.storage.sync.set({
+	//     [currentVideo]: JSON.stringify(
+	//       [...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)
+	//     ),
+	//   });
+	// };
 
-  // newVideoLoaded();
+	// newVideoLoaded();
 })();
